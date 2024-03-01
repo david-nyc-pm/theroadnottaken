c9c31520794fc42b6f29df5b51dbfe0eab5dbdf2
david-nyc-pm <drobertgardner@gmail.com>
2024-03-01 23:11:56 +0100

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+ myFunction = function() {
+    var first = document.getElementById("firstname").value;
+    var second = document.getElementById("lastname").value;
+    
+    document.getElementById("here").innerHTML = first+" "+second;
+}
